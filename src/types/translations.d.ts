e9cfc176d3142c046036e069fac957aef0f30b16
Mike15254 <mike8max10@gmail.com>
2024-12-16 21:54:37 +0300

    Add TypeScript types for translations module

---
+++ src/types/translations.d.ts
@@ -0,0 +1,6 @@
+declare module '@/translations/*' {
+    const content: {
+      [key: string]: any;
+    };
+    export default content;
+  }
---
+++ src/types/translations.ts
@@ -0,0 +1,22 @@
+export interface TranslationType {
+    nav: {
+      home: string;
+      about: string;
+      vision: string;
+      priorities: string;
+      why_okiya: string;
+      media: string;
+      mission: string;
+      strategic_plan: string;
+    };
+    hero: {
+      title: string;
+      subtitle: string;
+      tagline: string;
+      cta: {
+        join: string;
+        learn_more: string;
+      };
+    };
+  }
+  
